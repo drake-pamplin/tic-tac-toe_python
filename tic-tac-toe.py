8b1fb3fc530cf5fa6ad333917ed44b44f2973bd4
Drake Pamplin <drake.pamplin@gmail.com>
2021-10-25 17:04:01 -0500

    chore: removed graceful close at the end of the program for mac

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from platform import system as systemName
 
 referenceTable =(
@@ -129,5 +128,3 @@ if tie:
 # Stop the program from ending automatically
 input("Press \"Enter\" to end.")
 clearScreen()
-if (systemName().lower() == "darwin"):
-    os.system("osascript -e 'quit app \"{}\"".format(os.path.splitext(os.path.basename(__file__))[0]))
